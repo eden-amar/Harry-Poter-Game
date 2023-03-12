f80251be6ed5761040fc1a2036837c8a45edc0ee
Eden <eden2504@gmail.com>
2023-03-12 18:02:58 +0200

    new update

--- server/services/service-game.js
+++ server/services/service-game.js
@@ -1,8 +1,10 @@
 
 async function randomIcon(icon) {
     const keys = Object.keys(icon);
-    return icon.name[keys[ keys.length * Math.random() << 0]];
+    return icon.name[keys[ keys.length * Math.random() << 0]]; // not working
 }
+
+
 
 module.exports = {
     randomIcon
